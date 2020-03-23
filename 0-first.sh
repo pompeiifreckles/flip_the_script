dd55b6b57a985d5815f1c119d95f48a53acf88e7
Sid Sharma <sidhu97ss@gmail.com>
2020-03-23 14:59:03 +0530

    Beginner shell scripts for peers

---
+++ 0-first.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a comment
+
+echo "Hello     World"
+echo "Hello world"
+echo "Hello * world"
+echo Hello * world
+echo Hello 	world
+echo "Hello" world
+echo "hello " " world"
+echo "hello "*" world"
+echo `hello` world
+echo 'hello' world
